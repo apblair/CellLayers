a5d345bfe1efbe3015034997d7a2f3b3ae82c863
apblair <apblair@ucsc.edu>
2021-12-22 13:34:21 -0800

    Fig 1B adjust color bar

--- Env/runContainer-CellLayers_v0.2.sh
+++ Env/runContainer-CellLayers_v0.2.sh
@@ -1,4 +1,4 @@
-docker run -it -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes apblair/cell-layers:v0.2
+docker run -it -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v /home/apblair:/home/jovyan/work apblair/cell-layers:v0.2
 # -e JUPYTER_ENABLE_LAB=yes 
 # Inside the container run 
 # $ mamba install -c conda-forge jupyterlab
--- Env/runContainer.sh
+++ Env/runContainer.sh
@@ -1,2 +1,2 @@
-docker run --rm -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work bruneaulab/cell-layers:0.1
+docker run --rm -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v /home/apblair/:/home/jovyan/work bruneaulab/cell-layers:0.1
 
