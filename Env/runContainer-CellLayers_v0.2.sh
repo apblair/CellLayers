d354d5792da13a87958d75fb827aa5bf520877d0
apblair <andrew.blair@ucsf.edu>
2022-01-05 14:16:21 -0800

    success SetupCellLayers compute_modularity

--- Env/runContainer-CellLayers_v0.2.sh
+++ Env/runContainer-CellLayers_v0.2.sh
@@ -3,3 +3,4 @@ docker run -it -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v /home/apblair:/home/jo
 # Inside the container run 
 # $ mamba install -c conda-forge jupyterlab
 # $ jupyter lab --ip 0.0.0.0 --no-browser --allow-root
+# $ pip install chart_studio plotly
