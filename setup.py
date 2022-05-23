89133349ee6134455c78eaafe4289d91169cb3e6
Andrew Blair <andrew.blair@ucsf.edu>
2022-05-23 14:31:54 -0700

    v0.2 add nbsphinx dependecy for read the docs

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-required = ["matplotlib==3.5.1", "numpy==1.21.2", "pandas==1.4.1", "plotly==5.7.0", "chart-studio==1.1.0", "pandas==1.4.1", "scipy==1.7.3", "pytest"]
+required = ["matplotlib==3.5.1", "numpy==1.21.2", "pandas==1.4.1", "plotly==5.7.0", "chart-studio==1.1.0", "pandas==1.4.1", "scipy==1.7.3", "nbsphinx","pytest"]
 
 setup(name='CellLayers',
       description='Cell Layers is an interactive Sankey tool for the quantitative investigation of gene expression, coexpression, biological processes, and cluster integrity across clustering resolutions.',
