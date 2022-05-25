dd61a7888fd3e70c599e8e04536274b658374f4a
Andrew Blair <andrew.blair@ucsf.edu>
2022-05-24 22:04:42 -0700

    v0.2.1 update rtd figures

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,19 +45,18 @@ html_static_path = ['_static']
 
 
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-  import sphinx_rtd_theme
-  html_theme = 'sphinx_rtd_theme'
-  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-  html_style = 'css/custom.css'
-else:
-  html_context = { 
-    'css_files': [
-        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-        '_static/css/custom.css',
-    ],  
-  }   
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_style = 'css/custom.css'
+
+html_context = { 
+'css_files': [
+    'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+    'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+    '_static/css/custom.css',
+],  
+}   
 
   
 # -- Options for EPUB output
